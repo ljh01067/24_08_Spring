107cfc28c21030dc8461ca0282933d646bd9b219
ljh01067 <leejh01067@gmail.com>
2024-08-09 10:48:06 +0900

    0809-1

---
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "안녕하세요";
+	}
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가";
+	}
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public int showMain3() {
+		int a = 1;
+		int b =2;
+		
+		return a + b;
+	}
+}
