4affe579ae29873516186e34ad0ff4c461ac18ab
ljh01067 <leejh01067@gmail.com>
2024-08-12 16:50:32 +0900

    0812

---
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.service.MemberService;
+import com.example.demo.util.Ut;
+import com.example.demo.vo.Member;
+import com.example.demo.vo.ResultData;
+
+@Controller
+public class UsrMemberController {
+
+	@Autowired
+	private MemberService memberService;
+
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+			String email) {
+		if (Ut.isEmptyOrNull(loginId)) {
+			return ResultData.from("F-1", "loginId 입력 x");
+		}
+		if (Ut.isEmptyOrNull(loginPw)) {
+			return ResultData.from("F-2", "loginPw 입력 x");
+		}
+		if (Ut.isEmptyOrNull(name)) {
+			return ResultData.from("F-3", "name 입력 x");
+		}
+		if (Ut.isEmptyOrNull(nickname)) {
+			return ResultData.from("F-4", "nickname 입력 x");
+		}
+		if (Ut.isEmptyOrNull(cellphoneNum)) {
+			return ResultData.from("F-5", "cellphoneNum 입력 x");
+		}
+		if (Ut.isEmptyOrNull(email)) {
+			return ResultData.from("F-6", "email 입력 x");
+		}
+		ResultData doJoinRd = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+		if (doJoinRd.isFail()) {
+			return doJoinRd;
+		}
+		Member member = memberService.getMemberById((int) doJoinRd.getData1());
+		
+
+		return ResultData.newData(doJoinRd, member);
+	}
+
+}
--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -1,54 +1,23 @@
 package com.example.demo.repository;
-
-import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.vo.Article;
 
-@Component
-public class ArticleRepository {
-	private int lastArticleId;
-	public List<Article> articles;
-
-	public ArticleRepository() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
-	}
-
-	public Article writeArticle(String title, String body) {
-		int id = lastArticleId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastArticleId++;
-		return article;
-
-	}
-
-	public void deleteArticle(int id) {
-		Article article = getArticleById(id);
-		articles.remove(article);
-
-	}
-
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticleById(id);
-		article.setTitle(title);
-		article.setBody(body);
-
-	}
-
-	public Article getArticleById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	public List<Article> getArticles() {
-		return articles;
-	}
+@Mapper
+public interface ArticleRepository {
+//	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+	public void writeArticle(String title, String body);
+	@Delete("DELETE FROM article WHERE id = #{id}")
+	public void deleteArticle(int id);
+//	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
+	public void modifyArticle(int id, String title, String body);
+//	@Select("SELECT * FROM article WHERE id = #{id}")
+	public Article getArticleById(int id);
+//	@Select("SELECT * FROM article ORDER BY id DESC")
+	public List<Article> getArticles();
+	@Select("SELECT LAST_INSERT_ID();")
+	public int getLastInsertId();
 }
